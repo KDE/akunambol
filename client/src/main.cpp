1575d943121f5890585e92fd9f05c716bc419936
Dario Freddi <drf54321@gmail.com>
2008-10-08 15:48:02 +0000

    Copyrights
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Dario Freddi <drf54321@gmail.com>               *
+ *   Copyright (C) 2008 by Riccardo Iaconelli <riccardo@kde.org>           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ ***************************************************************************/
+
 #include "akunambol.h"
 #include <kapplication.h>
 #include <kaboutdata.h>
@@ -5,15 +25,17 @@
 #include <KDE/KLocale>
 
 static const char description[] =
-    I18N_NOOP("A KDE 4 Application");
+    I18N_NOOP("A Connector to Akonadi for the Funambol Mobile Server");
 
 static const char version[] = "0.1";
 
 int main(int argc, char **argv)
 {
     KAboutData about("akunambol", 0, ki18n("akunambol"), version, ki18n(description),
-                     KAboutData::License_GPL, ki18n("(C) 2007 Riccardo Iaconelli"), KLocalizedString(), 0, "riccardo@kde.org");
+                     KAboutData::License_GPL, ki18n("(C) 2008 Riccardo Iaconelli, Dario Freddi"),
+                     KLocalizedString(), 0, "riccardo@kde.org");
     about.addAuthor( ki18n("Riccardo Iaconelli"), KLocalizedString(), "riccardo@kde.org" );
+    about.addAuthor( ki18n("Dario Freddi"), KLocalizedString(), "riccardo@kde.org" );
     KCmdLineArgs::init(argc, argv, &about);
 
     KCmdLineOptions options;
