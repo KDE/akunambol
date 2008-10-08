fe808239128f7dd4bfb0be6c7199bc4a1dca183a
Dario Freddi <drf54321@gmail.com>
2008-10-08 16:02:51 +0000

    Starting GUI Design
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

--- client/src/akunambolview.cpp
+++ client/src/akunambolview.cpp
@@ -48,14 +48,7 @@ void akunambolView::switchColors()
 
 void akunambolView::settingsChanged()
 {
-    QPalette pal;
-    pal.setColor( QPalette::Window, Settings::col_background());
-    pal.setColor( QPalette::WindowText, Settings::col_foreground());
-    ui_akunambolview_base.kcfg_sillyLabel->setPalette( pal );
 
-    // i18n : internationalization
-    ui_akunambolview_base.kcfg_sillyLabel->setText( i18n("This project is %1 days old",Settings::val_time()) );
-    emit signalChangeStatusbar( i18n("Settings changed") );
 }
 
 #include "akunambolview.moc"
