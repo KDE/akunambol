34083688b41df56785393f190f743f37d9ad8ac5
Riccardo Iaconelli <riccardo@kde.org>
2009-06-18 15:15:51 +0000

    Implement some more sourcemanager
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- syncsource/sourcemanager.cpp
+++ syncsource/sourcemanager.cpp
@@ -7,11 +7,21 @@
 #include <client/DMTClientConfig.h>
 #include <spds/DefaultConfigFactory.h>
 
+#include "../config.h"
+
 USE_FUNAMBOL_NAMESPACE
+
+// Stolen from Funambol's fsync example.
+static StringBuffer generateDeviceID()
+{
+    StringBuffer devid;
+    devid.sprintf("%s-%ld", "akonadi", time(NULL));
+    return devid;
+}
 
 SourceManager::SourceManager()
 {
-    PlatformAdapter::init("Akunambol/Akonadi");
+    PlatformAdapter::init("Funambol/Akunambol");
     m_conf = new DMTClientConfig;
     initConfig();
 }
@@ -25,12 +35,19 @@ void SourceManager::initConfig()
 {
     if (!m_conf->read()) {
         AccessConfig *ac = DefaultConfigFactory::getAccessConfig();
-        ac->setUserAgent("Akunambol");
+        ac->setUserAgent("Akunambol " VERSION);
         m_conf->setAccessConfig(*ac);
 
         DeviceConfig *dc = DefaultConfigFactory::getDeviceConfig();
+        dc->setDevID(generateDeviceID());
+        dc->setMan("Akunambol");
+        dc->setSwv(VERSION);
+        m_conf->setDeviceConfig(*dc);
 
         SyncSourceConfig *sc = DefaultConfigFactory::getSyncSourceConfig("akonadi");
+        m_conf->setSyncSourceConfig(*sc);
+
+        m_conf->save();
         kDebug() << "wwaa";
         delete ac;
         delete dc;
