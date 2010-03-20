c91796e33cfd28c49b98b1b6d287c4b6a2d21b51
Riccardo Iaconelli <riccardo@kde.org>
2010-03-20 18:19:47 +0000

    reformat source well
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- syncsource/ContactsSource.h
+++ syncsource/ContactsSource.h
@@ -43,82 +43,83 @@
 #include <spds/SyncItem.h>
 #include <base/util/Enumeration.h>
 #include <Akonadi/Item>
+#include <QObject>
 
-class ContactsSource : public CacheSyncSource {
-
-public:
-
-
-    ContactsSource(const WCHAR* name, AbstractSyncSourceConfig *sc, KeyValueStore* cache);
-
-    virtual ~ContactsSource() {}
-
-    /**
-     * Get the content of an item given the key. It is used to populate
-     * the SyncItem before the engine uses it in the usual flow of the sync.
-     *
-     * @param key      the local key of the item
-     * @param size     OUT: the size of the content
-     */
-    void* getItemContent(StringBuffer& key, size_t* size);
+class ContactsSource : public CacheSyncSource
+{
+    public:
 
 
-    /**
-     * Returns an Enumeration containing the StringBuffer keys of all items.
-     *
-     * It is used both for the full sync, where all items are sent to the server,
-     * and for the fast sync to calculate the modification since the last
-     * successful sync.
-     *
-     * @return a newly allocated Enumeration that is free'd by the CacheSyncSource
-     *         CacheSyncSource.
-     *         Return NULL in case of error, an empty Enumeration
-     *         if there are no items.
-     */
-    Enumeration* getAllItemList();
+        ContactsSource(const WCHAR* name, AbstractSyncSourceConfig *sc, KeyValueStore* cache);
 
-    /**
-     * Called by the sync engine to add an item that the server has sent.
-     * The sync source is expected to add it to its database, then set the
-     * key to the local key assigned to the new item. Alternatively
-     * the sync source can match the new item against one of the existing
-     * items and return that key.
-     *
-     * @param item  the item as sent by the server
-     * @return      SyncML status code
-     */
-    int insertItem(SyncItem& item);
+        virtual ~ContactsSource() {}
 
-    /**
-     * Called by the sync engine to update an item that the source already
-     * should have. The item's key is the local key of that item.
-     *
-     * @param item  the item as sent by the server
-     * @return      SyncML status code
-     */
-    int modifyItem(SyncItem& item);
+        /**
+         * Get the content of an item given the key. It is used to populate
+         * the SyncItem before the engine uses it in the usual flow of the sync.
+         *
+         * @param key      the local key of the item
+         * @param size     OUT: the size of the content
+         */
+        void* getItemContent(StringBuffer& key, size_t* size);
 
-    /**
-     * Called by the sync engine to update an item that the source already
-     * should have. The item's key is the local key of that item, no data is
-     * provided.
-     *
-     * @param item  the item as sent by the server
-     * @return      SyncML status code
-     */
-    int removeItem(SyncItem& item);
 
-    int removeAllItems();
+        /**
+         * Returns an Enumeration containing the StringBuffer keys of all items.
+         *
+         * It is used both for the full sync, where all items are sent to the server,
+         * and for the fast sync to calculate the modification since the last
+         * successful sync.
+         *
+         * @return a newly allocated Enumeration that is free'd by the CacheSyncSource
+         *         CacheSyncSource.
+         *         Return NULL in case of error, an empty Enumeration
+         *         if there are no items.
+         */
+        Enumeration* getAllItemList();
 
-    void setAkonadiItems(Akonadi::Item::List items);
+        /**
+         * Called by the sync engine to add an item that the server has sent.
+         * The sync source is expected to add it to its database, then set the
+         * key to the local key assigned to the new item. Alternatively
+         * the sync source can match the new item against one of the existing
+         * items and return that key.
+         *
+         * @param item  the item as sent by the server
+         * @return      SyncML status code
+         */
+        int insertItem(SyncItem& item);
 
-private:
+        /**
+         * Called by the sync engine to update an item that the source already
+         * should have. The item's key is the local key of that item.
+         *
+         * @param item  the item as sent by the server
+         * @return      SyncML status code
+         */
+        int modifyItem(SyncItem& item);
 
-    const StringBuffer unfoldVCard(const char*);
-    QList< Akonadi::Item > m_items;
+        /**
+         * Called by the sync engine to update an item that the source already
+         * should have. The item's key is the local key of that item, no data is
+         * provided.
+         *
+         * @param item  the item as sent by the server
+         * @return      SyncML status code
+         */
+        int removeItem(SyncItem& item);
+
+        int removeAllItems();
+
+        void setAkonadiItems(Akonadi::Item::List items);
+
+    private:
+
+        const StringBuffer unfoldVCard(const char*);
+        QList< Akonadi::Item > m_items;
 
 
 };
 
 #endif
-// kate: indent-mode cstyle; space-indent on; indent-width 0; 
+// kate: indent-mode cstyle; space-indent on; indent-width 4; replace-tabs on; 
