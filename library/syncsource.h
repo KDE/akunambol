58a9ab1738da2467f405afc416bb3e79fdbb7004
Riccardo Iaconelli <riccardo@kde.org>
2011-01-24 17:02:24 +0100

    improve documentation very much
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- library/syncsource.h
+++ library/syncsource.h
@@ -38,7 +38,7 @@ class KDE_EXPORT SyncSource2 : public QObject
         void setConfig(SyncConfig2 *c);
         SyncConfig2* config();
         
-        /*
+        /**
          * Returns the text that should be shown to the user in order to activate
          * this control.
          * 
@@ -46,34 +46,36 @@ class KDE_EXPORT SyncSource2 : public QObject
          */
         virtual QString controlText() = 0;
         
-        /*
+        /**
          * Sets the status of the source as "locked", aka a sync is in progress.
-         * Will deadlock if already locked.
+         * Will deadlock if already locked. Also see documentation from QMutex, which is used
+         * internally.
          */
         void lock();
-        /*
-         * Unlocks the source.
+        /**
+         * Unlocks the source, so that it can be reused. Unlocks the QMutex, internally.
          */
         void unlock();
-        /*
-         * Operates in the same way as you can see in the QMutex documentation.
+        /**
+         * Tries to lock the syncsource. Operates in the same way as you can see in
+         * the QMutex documentation.
          */
         bool tryLock();
         
-        /*
+        /**
          * Reimplement to provide a configuration interface to the user.
          */
         virtual QWidget* configurationInterface() = 0;
         
     public slots:
-        /*
+        /**
          * Triggers a sync. It will fail with a meaningful error if the source is already locked.
          */
         void triggerSync();
         
     protected:
     
-        /*
+       /**
         * This function is called when the user, or any other event, triggers a sync.
         * This should launch the sync until finished, and should use the signals to notify the UI.
         */
@@ -84,8 +86,8 @@ class KDE_EXPORT SyncSource2 : public QObject
         void error(QString);
         void success();
         
-        /*
-         * Internally used
+        /**
+         * Internally used.
          */
         void started();
         
