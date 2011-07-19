c18663b30f6ded39bdf66127242bfbf9ca1593de
Riccardo Iaconelli <riccardo@kde.org>
2011-07-19 18:17:36 +0200

    add some sync item representation
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ library/funambolsyncitem.h
@@ -0,0 +1,44 @@
+/*
+    Akunambol - KDE client for syncing your data
+    Copyright (C) 2010-2011  Riccardo Iaconelli <riccardo@kde.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef FUNAMBOLSYNCITEM_H
+#define FUNAMBOLSYNCITEM_H
+
+#include "syncitem.h"
+#include <spds/SyncItem.h>
+#include <qstring.h>
+
+class FunambolSyncItem : public SyncItem
+{
+    public:
+        FunambolSyncItem();
+        virtual ~FunambolSyncItem();
+        Funambol::SyncItem *toFunambolItem();
+        
+        QString getKey();
+        QByteArray getData();
+        
+        static FunambolSyncItem fromFunambolItem(Funambol::SyncItem);
+        
+    private:
+        class Private;
+        Private *d;
+};
+
+#endif // FUNAMBOLSYNCITEM_H
