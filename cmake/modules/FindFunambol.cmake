c201dfb4fc2f89fae06daedd953f61ea94077d49
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2012-01-06 15:27:30 +0100

    Use pkgconfig and FPHSA

--- cmake/modules/FindFunambol.cmake
+++ cmake/modules/FindFunambol.cmake
@@ -1,28 +1,26 @@
 # - Try to find Funambol
 # Once done, this will define
 #
-#  Funambol_FOUND - system has Funambol
-#  Funambol_INCLUDE_DIRS - the Funambol include directories
-#  Funambol_LIBRARIES - link these to use Funambol
+#  FUNAMBOL_FOUND - system has Funambol
+#  Funambol_INCLUDE_DIR - the Funambol include directories
+#  Funambol_LIBRARY - link these to use Funambol
+#  Funambol_DEFINITIONS - Compiler flags required by Funambol
 
-include(LibFindMacros)
+if(NOT WIN32)
+  find_package(PkgConfig REQUIRED)
+  pkg_check_modules(PC_Funambol libfunambol)
+  set(Funambol_DEFINITIONS ${PC_Funambol_CFLAGS})
+endif()
 
-# Include dir
-find_path(Funambol_INCLUDE_DIR
-  # TODO: change that to a file that will always be there?
-  NAMES funambol/common/client/SyncClient.h
+find_path(Funambol_INCLUDE_DIR NAMES funambol/common/client/SyncClient.h
+  HINTS ${PC_Funambol_INCLUDEDIR}
 )
 
-# Hack?
-set(Funambol_INCLUDE_DIR ${Funambol_INCLUDE_DIR}/funambol/posix ${Funambol_INCLUDE_DIR}/funambol/common)
-
-# Finally the library itself
-find_library(Funambol_LIBRARY
-  NAMES funambol
+find_library(Funambol_LIBRARY NAMES funambol
+  HINTS ${PC_Funambol_LIBDIR}
 )
 
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-set(Funambol_PROCESS_INCLUDES Funambol_INCLUDE_DIR Funambol_INCLUDE_DIRS)
-set(Funambol_PROCESS_LIBS Funambol_LIBRARY Funambol_LIBRARIES)
-libfind_process(Funambol)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Funambol DEFAULT_MSG Funambol_INCLUDE_DIR Funambol_LIBRARY Funambol_DEFINITIONS)
 
+mark_as_advanced(Funambol_INCLUDE_DIR Funambol_LIBRARY Funambol_DEFINITIONS)
