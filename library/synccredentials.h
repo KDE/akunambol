57231205e4574a9dd57a09175fd87cd0151ba174
Riccardo Iaconelli <riccardo@kde.org>
2012-04-17 14:54:30 +0200

    proper include guard
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- library/synccredentials.h
+++ library/synccredentials.h
@@ -15,8 +15,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef SYNCCONFIG_H
-#define SYNCCONFIG_H
+#ifndef SYNCCREDENTIALS_H
+#define SYNCCREDENTIALS_H
 
 #include <QString>
 #include "akunambol_macros.h"
